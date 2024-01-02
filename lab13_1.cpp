0bf3fa7320b44a0863c59909d6fa0119730967bb
BallChon <Buranin_b@cmu.ac.th>
2024-01-02 16:29:00 +0700

    Lab13 finished eiei

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -16,4 +16,49 @@ int main(){
     cout << "\nMax = " << B[4];
     cout << "\nMin = " << B[5];
     return 0;
+} 
+
+void stat(const double src[],int N,double x[]){
+    //Arithmetic Mean 
+    double sum0 = 0;
+    for(int i=0; i<N; i++){
+        sum0 = sum0 + src[i];
+        x[0] = sum0/N;
+    }
+
+    //Standard Deviation
+    double sum1 = 0;
+    for(int i=0; i<N; i++){
+        sum1 = sum1 + pow(src[i]-x[0],2);
+        x[1] = sqrt(sum1/N);
+    }
+
+    // Geometric Mean
+    double sum2 = 1;
+    for(int i=0; i<N; i++){
+        sum2 = sum2*src[i];
+        x[2] = pow(sum2,1.0/N);
+    }
+
+    // Harmonic Mean
+    double sum3 = 0;
+    for(int i=0; i<N; i++){
+        sum3 = sum3+1/src[i];
+        x[3] = N/sum3;
+    }
+
+    //Max
+    double max = 0;
+    for(int i=0; i<N; i++){
+        if(src[i] > max)max=src[i];
+    }
+    x[4] = max;
+
+    //min
+    double min = src[0];
+    for(int i=0; i<N; i++){
+        if(src[i] < min)min=src[i];
+    }
+    x[5] = min;
+
 }
